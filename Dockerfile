62b82aec4a2d24b31c13ef5cb0c397ab79278269
Ameya Mahankal <ameya.mahankal@gmail.com>
2023-12-07 13:25:01 +0100

    DockerLoginTest

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
-FROM node:alpine
+# Use an official Node.js runtime as a base image
+FROM node:14
 
-WORKDIR /usr/src/app
+# Create and set the working directory
+WORKDIR /app
 
+# Copy package.json and package-lock.json to the working directory
 COPY package*.json ./
 
+# Install the application dependencies
 RUN npm install
 
+# Copy the application code to the working directory
 COPY . .
 
-EXPOSE 3000
+# Expose the port that your application is listening on
+EXPOSE 3001
 
+# Start the application when the container starts
 CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -1,34 +1,22 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const mongoose = require('mongoose')
-const mongoString = "mongodb+srv://ameyamahankal:4mq7FaDOD8LzQg18@cluster0.sjpz1jh.mongodb.net/"
-const aModel = require('./ArtifactModel')
+const net = require('net');
 
-mongoose.connect(mongoString);
-const database = mongoose.connection
+// TCP Server
+const server = net.createServer((socket) => {
+    console.log('Client connected');
 
-database.on('error', (error) => {
-    console.log(error)
-})
+    socket.on('data', (data) => {
+        console.log(`Received data: ${data}`);
+        // Process the data as needed
+    });
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
-
-app.get('/', (req, res) => {
-    res.send('Hello,I updated World!');
+    socket.on('end', () => {
+        console.log('Client disconnected');
+    });
 });
 
-app.get('/getAllArtifacts', async (req, res) => {
-    try {
-        const allArtifacts = await aModel.find();
-        res.json(allArtifacts);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+const tcpPort = process.env.TCP_PORT || 3001; // Choose a different port for TCP, if needed
+
+server.listen(tcpPort, () => {
+    console.log(`TCP server listening on port ${tcpPort}`);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
